b85a05720229904871112b6bf310f4f642182b6a
Simon Andrews <simon@hubblehq.com>
2020-02-16 21:29:49 +0000

    Remove unnecessary boilerplate

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,19 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>Page not found</h1>
+    <p>Hm… that page doesn’t exist.</p>
+
+    <ul>
+      <li>
+        The page you’re looking for may have been moved elsewhere. Try searching
+        for the content you expected.
+      </li>
+      <li>This page may have momentarily disappeared. Try reloading.</li>
+      <li>
+        You may have followed an inactive link. Check with whomever provided it.
+      </li>
+    </ul>
   </Layout>
 )
 
