d2559632f4d08d468fecfc6e5eb7fbb1c6be1057
Simon Andrews <me@simonandrews.ca>
2020-06-04 22:04:18 +0100

    Integrate new Jobs component on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Heading from "../components/Heading"
+import Jobs, { Job } from "../components/Jobs"
 import Layout from "../components/Layout"
 import Section from "../components/Section"
 import SEO from "../components/seo"
@@ -12,7 +12,9 @@ export default function IndexPage() {
       allWorkJson {
         edges {
           node {
+            brandColor
             company
+            isFocus
             logo {
               type
               src
@@ -33,48 +35,11 @@ export default function IndexPage() {
 
       <Section>
         <p>It’s a website. On The Internet.</p>
-
-        <ul>
-          {data.allWorkJson.edges.map(
-            ({
-              node: {
-                company,
-                logo: { type: logoType, src: logoSrc },
-                positions,
-              },
-            }) => (
-              <li key={company}>
-                {logoType &&
-                  logoSrc &&
-                  (function () {
-                    if (logoType === "raster") {
-                      return (
-                        <img
-                          src={require(`../images/${logoSrc}`)}
-                          style={{ maxWidth: "200px" }}
-                        />
-                      )
-                    } else {
-                      const Logo = require(`../images/${logoSrc}`)
-                      return (
-                        <Logo height={null} style={{ maxWidth: "200px" }} />
-                      )
-                    }
-                  })()}
-                <Heading element="h2" type="md">
-                  {company}
-                </Heading>
-                <ul>
-                  {positions.map(({ time, title }) => (
-                    <li key={time}>
-                      <strong>{title}</strong>, {time}
-                    </li>
-                  ))}
-                </ul>
-              </li>
-            )
-          )}
-        </ul>
+        <Jobs>
+          {data.allWorkJson.edges.map(({ node }) => (
+            <Job key={node.company} {...node} />
+          ))}
+        </Jobs>
       </Section>
     </Layout>
   )
