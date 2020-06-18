fb12fd6829e7a5925d737593398f55370d1ce666
Simon Andrews <me@simonandrews.ca>
2020-06-18 12:20:23 +0100

    Add links to Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,52 @@
 import React from "react"
+import { ArrowUp, GitHub, Instagram, Linkedin, Twitter } from "react-feather"
 
 import Section from "../Section"
 import Surface from "../Surface"
+import TextWithIcon from "../TextWithIcon"
 
-export default function Footer() {
+import { link, links, wrapper } from "./Footer.module.css"
+
+export default function Footer({
+  links: { github, instagram, linkedin, twitter },
+}) {
   return (
     <footer>
       <Surface backgroundColorType="brand-primary">
-        <Section verticalPadding="lg">© {new Date().getFullYear()}</Section>
+        <Section verticalPadding="lg">
+          <div className={wrapper}>
+            <span>© {new Date().getFullYear()}</span>
+            <ul className={links}>
+              <li className={link}>
+                <a href={github}>
+                  <GitHub />
+                </a>
+              </li>
+              <li className={link}>
+                <a href={linkedin}>
+                  <Linkedin />
+                </a>
+              </li>
+              <li className={link}>
+                <a href={instagram}>
+                  <Instagram />
+                </a>
+              </li>
+              <li className={link}>
+                <a href={twitter}>
+                  <Twitter />
+                </a>
+              </li>
+              <li className={link}>
+                <a href="#top">
+                  <TextWithIcon iconAlignment="end" icon={ArrowUp}>
+                    Top of page
+                  </TextWithIcon>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </Section>
       </Surface>
     </footer>
   )
---
+++ src/components/Footer/Footer.stories.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+import Footer from "."
+
+export default {
+  title: "Components|Footer",
+}
+
+export const Primary = () => (
+  <Footer
+    links={{
+      github: "https://www.github.com/simon360",
+      instagram: "https://www.instagram.com/simon360",
+      linkedin: "https://www.linkedin.com/in/sadl-uk/",
+      twitter: "https://www.twitter.com/simon360",
+    }}
+  />
+)
