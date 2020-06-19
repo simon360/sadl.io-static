ff13a305e7f2922164b8a77d32c611e0cda46d37
Simon Andrews <me@simonandrews.ca>
2020-06-19 22:31:42 +0100

    Add better Gatsby support to Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -11,6 +11,19 @@ module.exports = {
       if (config.module.rules[i].test.test("file.css")) {
         config.module.rules[i].exclude = /\.module\.css$/
       }
+
+      if (config.module.rules[i].test.test("file.js")) {
+        // Gatsby files need transpiling
+        config.module.rules[i].exclude = [/node_modules\/(?!(gatsby)\/)/]
+
+        // Language options for Gatsby
+        config.module.rules[i].use[0].options.plugins = [
+          // use @babel/plugin-proposal-class-properties for class arrow functions
+          require.resolve("@babel/plugin-proposal-class-properties"),
+          // use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
+          require.resolve("babel-plugin-remove-graphql-queries"),
+        ]
+      }
     }
 
     // Add in CSS module support
@@ -28,6 +41,9 @@ module.exports = {
         },
       ],
     })
+
+    // Prefer Gatsby ES6 entrypoint (module) over commonjs (main) entrypoint
+    config.resolve.mainFields = ["browser", "module", "main"]
 
     return config
   },
